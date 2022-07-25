5e7363d41019703869f679cc1952398f1dfa2817
Cozhee <davis.r.cody@gmail.com>
2022-07-25 15:31:36 -0700

    added styles

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import useForm from '../../hooks/form.js';
+import {SettingsContext} from "../../Context/Settings";
+import Header from "../header/Header";
+import './todo.css'
 
 import { v4 as uuid } from 'uuid';
 
 const ToDo = () => {
+
+  const settings = useContext(SettingsContext)
 
   const [defaultValues] = useState({
     difficulty: 4,
   });
   const [list, setList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
+  const [start, setStart] = useState(0)
+  const [end, setEnd] = useState(settings.itemsPerScreen)
   const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
 
   function addItem(item) {
     item.id = uuid();
-    item.complete = false;
+    item.complete = settings.completed;
     console.log(item);
     setList([...list, item]);
   }
@@ -27,7 +34,7 @@ const ToDo = () => {
   function toggleComplete(id) {
 
     const items = list.map( item => {
-      if ( item.id == id ) {
+      if ( item.id === id ) {
         item.complete = ! item.complete;
       }
       return item;
@@ -35,6 +42,17 @@ const ToDo = () => {
 
     setList(items);
 
+  }
+
+
+  function handleNextPageChange () {
+    setStart((start) => start + 3)
+    setEnd((end) => end + 3)
+  }
+
+  function handlePreviousPageChange () {
+    setStart((start) => start - 3)
+    setEnd((end) => end - 3)
   }
 
   useEffect(() => {
@@ -45,44 +63,45 @@ const ToDo = () => {
 
   return (
     <>
-      <header>
-        <h1>To Do List: {incomplete} items pending</h1>
-      </header>
+      <Header incomplete={incomplete}/>
 
-      <form onSubmit={handleSubmit}>
+      <h2 className="todo-title">Add To Do Item</h2>
 
-        <h2>Add To Do Item</h2>
+      <form onSubmit={handleSubmit} className="todo-form">
 
-        <label>
+        <label className="todo-input">
           <span>To Do Item</span>
           <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
         </label>
 
-        <label>
+        <label className="todo-input">
           <span>Assigned To</span>
           <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
         </label>
 
-        <label>
+        <label className="todo-input">
           <span>Difficulty</span>
           <input onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
         </label>
 
-        <label>
-          <button type="submit">Add Item</button>
+        <label className="todo-input">
+          <button className="bp4-button bp4-icon-add" type="submit">Add Item</button>
         </label>
       </form>
 
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
+      <div className="todo-list">
+        {list.slice(start, end).map(item => (
+            <div key={item.id} className="bp4-card bp4-elevation-2">
+              <p>{item.text}</p>
+              <p><small>Assigned to: {item.assignee}</small></p>
+              <p><small>Difficulty: {item.difficulty}</small></p>
+              <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+            </div>
+        ))}
+      </div>
 
+      {end < list.length && <button className="bp4-button" onClick={handleNextPageChange}>Next Page</button>}
+      {start > 0 && <button className="bp4-button" onClick={handlePreviousPageChange}>Previous Page</button>}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import SettingsProvider from "./Context/Settings";
 
 import App from './app.js';
 
 function Main() {
-    return <App />;
+    return (
+        <SettingsProvider>
+            <App />
+        </SettingsProvider>
+    ) ;
 }
 
 const container = document.getElementById('root');
